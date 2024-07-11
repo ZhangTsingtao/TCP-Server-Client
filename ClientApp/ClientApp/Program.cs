46c0e338d14a854be050e0888f4d83cc189f92b8
Tsingtao Zhang <rickzhangqt@gmail.com>
2024-07-11 17:10:46 -0400

    Initial Push

---
+++ ClientApp/ClientApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static Socket socket;
+        const int BUFFER_SIZE = 1024;
+        static byte[] readBuff = new byte[BUFFER_SIZE];
+        public static void Main(string[] args)
+        {
+            Console.Write("Client Hello World!!!\n");
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect("129.21.196.206", 5050);
+            int num = 0;
+            while (true)
+            {
+                String str = "Client:Hello Heiren!!!";
+                if (num == 0) num++;
+                else str = Console.ReadLine();
+                if (str.Equals("exit"))
+                    break;
+                byte[] bytes = System.Text.Encoding.Default.GetBytes(str);
+                socket.Send(bytes);
+                int count = socket.Receive(readBuff);
+                str = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
+                Console.WriteLine(str);
+
+            }
+
+            socket.Close();
+
+        }
+    }
+}
---
+++ ServerApp/ServerApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class SocketServer
+{
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 5050);
+        server.Start();
+
+        Console.WriteLine("Server started...");
+        TcpClient client = server.AcceptTcpClient();
+        NetworkStream stream = client.GetStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+        {
+            string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received: " + message);
+        }
+
+        stream.Close();
+        client.Close();
+        server.Stop();
+    }
+}
